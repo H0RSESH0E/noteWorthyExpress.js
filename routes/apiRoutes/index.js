00818d628d0192f0e39e2d265da43212969b95f2
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-03-11 14:36:08 -0500

    random id creation in place and files saving to db.json

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
-const { validateNote, createNewNote, writeAfterDelete } = require('../../lib/notes.js');
+const generatePassword = require('../../lib/uniqueId.js')
+const { validateNote, createNewNote, writeDbJSON } = require('../../lib/notes.js');
 
 const { notesArr } = require('../../db/db.json');
 console.log(notesArr);
@@ -11,28 +12,21 @@ router.get('/notes', (req, res) => {
     //   results = filterByQuery(req.query, results);
     // }
     res.json(notesArr);
-  });
-    
-  router.post('/notes', (req, res) => {
-    console.log("Here! ", req.body)
-    req.body.id = notesArr.length.toString();
-  
-    if (!validateNote(req.body)) {
-      res.status(400).send('The note is not in the correct format.');
-    } else {
-      const note = createNewNote(req.body, notesArr);
-      res.json(note);
-    }
-  });
-
-  router.delete('/notes/:idNumber', (req, res) => {
-    console.log(req.params.idNumber);
-    console.log("Deleting... ", req.params.idNumber)
-    writeAfterDelete(req.params.idNumber, notesArr)
-    .then(() => {
-        return res.json(req.params.idNumber)
-    });
-    
 });
-  
-  module.exports = router; 
+
+
+router.post('/notes', (req, res) => {
+    req.body.id = generatePassword();
+    notesArr.push(req.body);
+    console.log(notesArr);
+    res.json(notesArr);
+}
+);
+
+
+router.delete('/notes/:idNumber', (req, res) => {
+
+    writeDbJSON(req.params.idNumber, notesArr)
+});
+
+module.exports = router; 
